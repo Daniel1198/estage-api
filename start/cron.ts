fcb3bdd50f5a2db4702de812d39c609b62c6d1bf
Daniel1198 <kradaniel40@gmail.com>
2024-10-23 19:24:36 +0000

    gestion des envois automatique de mails

--- start/cron.ts
+++ start/cron.ts
@@ -27,44 +27,51 @@ async function updateStagiaireStatut(stage: Stage) {
     const stagiaire = await Stagiaire.query().where({ id: stage.stagiaireId }).preload('stages', (p) => p.preload('responsable').preload('entite').orderBy('fin', 'desc').first()).first()
     await Stagiaire.query().where({ id: stage.stagiaireId }).update({ statut: stagiaire?.stages[0].statut })
     if (moment(stage.fin).diff(date, 'days') === 7) {
-        cron.schedule('30 12 * * *', async () => {
-            sendReminderMail(7, stagiaire?.email!, 'stagiaire')
-            sendReminderMail(7, stagiaire?.stages[0].responsable.email!, 'responsable', stagiaire!)
-        })
+        sendReminderMail(7, stagiaire?.email!, 'stagiaire', stagiaire!)
+        sendReminderMail(7, stagiaire?.stages[0].responsable.email!, 'responsable', stagiaire!)
+        sendReminderMail(7, '', 'rh', stagiaire!)
     }
     if (moment(stage.fin).diff(date, 'days') === 3) {
-        cron.schedule('30 17 * * *', async () => {
-            sendReminderMail(3, stagiaire?.email!, 'stagiaire')
-            sendReminderMail(3, stagiaire?.stages[0].responsable.email!, 'responsable', stagiaire!)
-        })
+        sendReminderMail(3, stagiaire?.email!, 'stagiaire', stagiaire!)
+        sendReminderMail(3, stagiaire?.stages[0].responsable.email!, 'responsable', stagiaire!)
+        sendReminderMail(3, '', 'rh', stagiaire!)
     }
-    if (moment(stage.fin).diff(date, 'days') === 0) {
-        cron.schedule('30 12 * * *', async () => {
-            sendReminderMail(0, stagiaire?.email!, 'stagiaire')
-            sendReminderMail(0, stagiaire?.stages[0].responsable.email!, 'responsable', stagiaire!)
-        })
+    if (moment(stage.fin).diff(date, 'days') === 1) {  
+        sendReminderMail(1, stagiaire?.email!, 'stagiaire', stagiaire!)
+        sendReminderMail(1, stagiaire?.stages[0].responsable.email!, 'responsable', stagiaire!)
+        sendReminderMail(1, '', 'rh', stagiaire!)
     }
 }
 
 // Fonction qui envoie un mail
-async function sendReminderMail(daysBeforeEnd: number, email: string, personne: 'stagiaire' | 'responsable', stagiaire?: Stagiaire) {
+async function sendReminderMail(daysBeforeEnd: number, email: string, personne: 'stagiaire' | 'responsable' | 'rh', stagiaire?: Stagiaire) {
     await mail.send((message) => {
-        message
-            .to(email)
-            .subject('Rappel : Fin de stage')
-            .text(
-                personne ==='stagiaire' ? 
-                ( daysBeforeEnd !== 0 ? `Rappel : Fin de votre stage dans ${daysBeforeEnd} jours` : `Rappel : Votre stage finit aujourd'hui`) : 
-                ( daysBeforeEnd !== 0 ? `Rappel : le stagiaire ${stagiaire?.nom} ${stagiaire?.prenom} à la direction ${stagiaire?.stages[0].entite.intitule} termine son stage dans ${daysBeforeEnd} jours` : 
-                    `Rappel : le stagiaire ${stagiaire?.nom} ${stagiaire?.prenom} à la direction ${stagiaire?.stages[0].entite.intitule} termine son stage aujourd'hui.`)
-            )
+        if (personne === 'stagiaire') {
+            if (daysBeforeEnd !== 0) {
+                message.to(email!).subject('Rappel : Fin de stage').htmlView('emails/end_internship_for_intern', { stagiaire: stagiaire?.nom + ' ' + stagiaire?.prenom, dateFin: moment(stagiaire?.stages[0].fin).format('DD.MM.YYYY')})
+            } else {
+                message.to(email!).subject('Rappel : Fin de stage').htmlView('emails/end_internship_for_intern_today', { stagiaire: stagiaire?.nom + ' ' + stagiaire?.prenom })
+            }
+        } else if (personne === 'responsable') {
+            if (daysBeforeEnd !== 0) {
+                message.to(email!).cc(['ange.kra@rti.ci']).subject('Rappel : Fin de stage').htmlView('emails/end_internship_for_manager', { responsable: stagiaire?.stages[0].responsable.nom + ' ' + stagiaire?.stages[0].responsable.prenom, stagiaire: stagiaire?.nom +' '+ stagiaire?.prenom, dateFin: moment(stagiaire?.stages[0].fin).format('DD.MM.YYYY')})
+            } else {
+                message.to(email!).cc(['ange.kra@rti.ci']).subject('Rappel : Fin de stage').htmlView('emails/end_internship_for_manager_today', { responsable: stagiaire?.stages[0].responsable.nom + ' ' + stagiaire?.stages[0].responsable.prenom,stagiaire: stagiaire?.nom +' '+ stagiaire?.prenom })
+            }
+        } else if (personne === 'rh') {
+            if (daysBeforeEnd!== 0) {
+                message.to('ange.kra@rti.ci').subject('Rappel : Fin de stage').htmlView('emails/end_internship_for_rh', { responsable: 'ASSISTANT RH', stagiaire: stagiaire?.nom + ' ' + stagiaire?.prenom, dateFin: moment(stagiaire?.stages[0].fin).format('DD.MM.YYYY'), nbrJours: daysBeforeEnd })
+            } else {
+                message.to('ange.kra@rti.ci').subject('Rappel : Fin de stage').htmlView('emails/end_internship_for_rh_today', { responsable: 'ASSISTANT RH', stagiaire: stagiaire?.nom + ' ' + stagiaire?.prenom })
+            }
+        }
     })
 }
 
 // Planification pour exécuter la tâche tous les jours à minuit
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
     const stagesActifs = await Stage.all()
-    stagesActifs.forEach(async (stage) => {
+    stagesActifs.map(async (stage) => {
         if (stage.statut !== 'TERMINE') 
             await changeStatus(stage)
     })
